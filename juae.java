0cb2b71168579e7bc467bc71e061fbe815d8134e
“684259052@webmail.npru.ac.th” <>
2025-07-25 15:27:05 +0700

    test scanncer

--- juae.java
+++ juae.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class juae {
   public static void main (String [] args ){
-  int num1 = 4;
-  int num2 = 6;
+//   int num1 = 4;
+//   int num2 = 6;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter num1:");
+    int num1 = scanner.nextInt();
+    System.out.print("Enter num2 :");
+    int num2= scanner.nextInt();
+
   int sum = num1 +num2; 
-  System.out.println( " Sum of "+num1+"and"+num2+" ="+sum );
+  System.out.println( "Sum = "+sum );
+  
   }
  
 
