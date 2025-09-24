3868dca3a1d6c9b31b7cab5e8c73da25f7209c5a
SahapatBUDSAYAKUL <684259052@webmail.npru.ac.th>
2025-09-24 19:19:40 +0700

    memmem

---
+++ membership.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class membership {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter membership level (Silver, Gold, Platinum): ");
+        String level = sc.nextLine();
+        System.out.print("Enter total purchase amount: ");
+        double total = sc.nextDouble();
+
+        double discountRate;
+
+        
+        if (level.equalsIgnoreCase("Silver")) {
+            discountRate = 0.05;
+        } else if (level.equalsIgnoreCase("Gold")) {
+            discountRate = 0.10;
+        } else if (level.equalsIgnoreCase("Platinum")) {
+            discountRate = 0.15;
+        } else {
+            discountRate = 0.0;
+        }
+
+        
+        double discount = total * discountRate;
+        double finalPrice = total - discount;
+
+    
+        System.out.println("\n--- Receipt ---");
+        System.out.println("Member level: " + level);
+        System.out.println("Total purchase: " + total);
+        System.out.println("Discount: " + discount);
+        System.out.println("Final price: " + finalPrice);
+
+    }
+}
